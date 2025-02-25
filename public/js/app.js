49ee867ced3b81709034752bf61998f6f0027013
Alexandre Lerosier <alex.lerosier@gmail.com>
2025-02-25 12:28:19 +0100

    fix : new name for github repo

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
-import Config from './../../app/Config.js';
+import root from './../../app/root.js';
 
 // Load the header
-fetch(`${Config.root()}/pages/partials/header.html`)
+fetch(`${root()}/pages/partials/header.html`)
 .then(response => response.text())
 .then(header => {
-    header = header.replaceAll('__PROFIL__', Config.root() + '/pages/profil.html');
-    header = header.replaceAll('__PROJECTS__', Config.root() + '/pages/project/index.html');
-    header = header.replaceAll('__CONTACT__', Config.root() + '/pages/contact.html');
+    header = header.replaceAll('__PROFIL__', root() + '/pages/profil.html');
+    header = header.replaceAll('__PROJECTS__', root() + '/pages/project/index.html');
+    header = header.replaceAll('__CONTACT__', root() + '/pages/contact.html');
 
     document.getElementById('header').innerHTML = header;
 })
 .catch(error => console.error(error));
 
 // Load the footer
-fetch(`${Config.root()}/pages/partials/footer.html`)
+fetch(`${root()}/pages/partials/footer.html`)
 .then(response => response.text())
 .then(data => {
     document.getElementById('footer').innerHTML = data;
--- public/js/project/index.js
+++ public/js/project/index.js
@@ -1,4 +1,4 @@
-import Config from './../../../app/Config.js';
+import root from './../../../app/root.js';
 import projects from './../../../storage/db/projects.js';
 
 const cards = document.getElementById('cards');
@@ -11,8 +11,8 @@ projects.forEach(project => {
     let card = template;
 
     card = card.replaceAll("__TITLE__", project.title);
-    card = card.replaceAll("__LINK__", `${Config.root()}/pages/project/details.html?id=${project.id}`);
-    card = card.replaceAll("__CARD__", `${Config.root()}/storage/projects/${project.id}/card.png`);
+    card = card.replaceAll("__LINK__", `${root()}/pages/project/details.html?id=${project.id}`);
+    card = card.replaceAll("__CARD__", `${root()}/storage/projects/${project.id}/card.png`);
 
     cards.innerHTML += card;
 });
